d13b25dff22ebca8bf4ea2996d832c9610848b7d
Phone Thiha Kyaw <mlsdphonethk@gmail.com>
2024-01-08 01:04:08 +0800

    feat: add benchmark module

---
+++ include/anyangle_planners/benchmark/config.hpp
@@ -0,0 +1,98 @@
+// MIT License
+
+// Copyright (c) 2024 Phone Thiha Kyaw
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "anyangle_planners/registration.hpp"
+
+namespace anyangle {
+namespace benchmark {
+namespace config {
+
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace env_type {
+struct Base
+{
+  /// @brief environment type representation (INVALID = 0, GRIDMAP = 1)
+  EnvironmentType env_type;
+
+  /// @brief Distance metric to use during search
+  /// INVALID = 0, EUCLIDEAN = 1, MANHATTAN = 2, OCTILE = 3
+  DistanceMetric distance_metric;
+};
+
+struct GridMap : Base
+{
+  /// @brief Type of grid connectivity
+  /// FOUR  = 0 (four-connected grid)
+  /// EIGHT = 1 (eight-connected grid)
+  grid_map::Connectivity connectivity;
+};
+}  // namespace env_type
+
+namespace env_id {
+struct Base
+{
+  /// @brief registered environment IDs (INVALID = 0, MovingAILabScenario = 1)
+  EnvironmentID env_id;
+};
+
+struct MovingAILabScenario : Base
+{
+  /// @brief Name of the moving AI Lab scenario file
+  std::string scenario_file_name;
+};
+}  // namespace env_id
+
+/**
+ * @brief Params related to environmental representation of the planning experiment
+ *
+ */
+struct Environment
+{
+  std::shared_ptr<env_type::Base> env_type_params;
+  std::shared_ptr<env_id::Base> env_id_params;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+struct Config
+{
+  /// @brief Path to this benchmark config file
+  std::string config_file_path;
+
+  /// @brief list of planners to use during benchmarking
+  std::vector<PlannerID> planners;
+
+  /// @brief params related to environmental representation of the planning experiment
+  Environment env_params;
+
+  /// @brief Number of experiments to run
+  unsigned int num_of_runs;
+};
+
+void from_json(const nlohmann::json& j, Config& config);
+
+}  // namespace config
+}  // namespace benchmark
+}  // namespace anyangle
---
+++ src/benchmark/src/config.cpp
@@ -0,0 +1,75 @@
+// MIT License
+
+// Copyright (c) 2024 Phone Thiha Kyaw
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "anyangle_planners/benchmark/config.hpp"
+
+#include <iostream>
+
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+namespace anyangle {
+namespace benchmark {
+namespace config {
+
+void from_json(const json& j, Config& config)
+{
+  // update planners
+  if (j.contains("planners"))
+  {
+    auto j_planners = j["planners"];
+    for (const auto& j_id : j_planners)
+    {
+      config.planners.push_back(j_id.template get<anyangle::PlannerID>());
+    }
+  }
+
+  // update environment config
+  if (j.contains("environment"))
+  {
+    // get environment type and id
+    auto env_type = j["environment"]["type"].template get<anyangle::EnvironmentType>();
+    auto env_id = j["environment"]["id"].template get<anyangle::EnvironmentID>();
+
+    if (env_type == anyangle::EnvironmentType::GRIDMAP)
+    {
+      config.env_params.env_type_params = std::make_shared<env_type::GridMap>();
+      config.env_params.env_type_params->env_type = env_type;
+    }
+
+    if (env_id == anyangle::EnvironmentID::MovingAILabScenario)
+    {
+      config.env_params.env_id_params = std::make_shared<env_id::MovingAILabScenario>();
+      config.env_params.env_id_params->env_id = env_id;
+    }
+  }
+
+  if (j.contains("num_of_runs"))
+  {
+    config.num_of_runs = j["num_of_runs"].template get<unsigned>();
+  }
+}
+
+}  // namespace config
+}  // namespace benchmark
+}  // namespace anyangle
